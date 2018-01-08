2471f3c2911b2f3bb6a1384c93e9a7a4382c4af2
Tim <timmeyran@gmail.com>
2018-01-09 00:26:19 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String sentence;
+        String modifiedSentence;
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        Socket clientSocket = null;
+        try {
+            clientSocket = new Socket("raspberrpi", 6789);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            sentence = inFromUser.readLine();
+            outToServer.writeBytes(sentence + '\n');
+            modifiedSentence = inFromServer.readLine();
+            System.out.println("FROM SERVER: " + modifiedSentence);
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
